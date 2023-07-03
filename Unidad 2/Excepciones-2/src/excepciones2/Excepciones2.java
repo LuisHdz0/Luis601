6795cb430d3bf2d14c684536e2f94bb0a3de6b14
LUIS <luisennez@gmail.com>
2023-07-02 20:35:56 -0600

    Se adjunta  programa de Exeption

---
+++ Unidad 2/Excepciones-2/src/excepciones2/Excepciones2.java
@@ -0,0 +1,25 @@
+
+package excepciones2;
+
+import javax.swing.JOptionPane;
+
+
+public class Excepciones2 {
+
+    public static void main(String[] args) {
+ int x,y;
+       
+        try{
+         x= Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese numero 1","Solicito datos",3));
+         y= Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese numero 2","Solicito datos",3));
+         JOptionPane.showMessageDialog(null,"La Divicion de   "+x+"  /  "+y+" es "+(x/y)," Motrando resultado",1);
+        }
+        catch (NumberFormatException n){
+            System.out.println(n.getMessage() + "  No es un numero ENTERO");
+        }
+         catch (ArithmeticException a){
+            System.out.println(a.getMessage() + "    No se puede dividir entre 0");
+        }
+    }
+    
+}
